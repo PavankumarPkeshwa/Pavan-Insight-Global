96f658f3fa2b2be72689f12887a07e6edad23efd
Pavan Kumar P <98679889+PavankumarPkeshwa@users.noreply.github.com>
2025-07-28 23:06:23 +0530

    Delete ML-Model/utils directory

--- ML-Model/utils/preprocess.py
+++
@@ -1,9 +0,0 @@
-import re
-
-def clean_text(text):
-    if not text or not isinstance(text, str):
-        return ""
-
-    text = re.sub(r"\s+", " ", text)  # remove extra spaces/newlines
-    text = re.sub(r"http\S+", "", text)  # remove URLs
-    return text.strip()
